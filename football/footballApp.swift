3c8e9e21de906f7342e463770409287daba82668
Alec Newman <alecjnewman@gmail.com>
2025-01-19 14:14:11 -0500

    Initial Commit

---
+++ football/footballApp.swift
@@ -0,0 +1,17 @@
+//
+//  footballApp.swift
+//  football
+//
+//  Created by Alec Newman on 1/19/25.
+//
+
+import SwiftUI
+
+@main
+struct footballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
